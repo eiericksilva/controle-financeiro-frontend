c374315f3f32e403318389018e7b3f8d5554786c
Erick Silva <ericksilvaredes@gmail.com>
2024-05-13 18:12:09 -0300

    feat: category page

--- src/routes/category.jsx
+++ src/routes/category.jsx
@@ -1,22 +1,167 @@
+import Button from "../components/button/button";
+
 export default function Category() {
-  const category = {
-    id: 1,
-    name: "Category A",
-    categoryType: "INCOME",
-    /*       subcategories: "lista de Subcategorias", */
-  };
+  const incomeCategories = [
+    {
+      id: 1,
+      name: "Category 1",
+      categoryType: "INCOME",
+      subcategories: [
+        {
+          id: 1,
+          name: "Subcategory 1.1",
+        },
+        {
+          id: 2,
+          name: "Subcategory 1.2",
+        },
+        {
+          id: 3,
+          name: "Subcategory 1.3",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Category 2",
+      categoryType: "INCOME",
+      subcategories: [
+        {
+          id: 1,
+          name: "Subcategory 2.1",
+        },
+        {
+          id: 2,
+          name: "Subcategory 2.2",
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Category 3",
+      categoryType: "INCOME",
+      subcategories: [
+        {
+          id: 1,
+          name: "Subcategory 3.1",
+        },
+        {
+          id: 2,
+          name: "Subcategory 3.2",
+        },
+        {
+          id: 3,
+          name: "Subcategory 3.3",
+        },
+        {
+          id: 2,
+          name: "Subcategory 3.4",
+        },
+      ],
+    },
+  ];
+  const expenseCategories = [
+    {
+      id: 1,
+      name: "Category 1",
+      categoryType: "EXPENSE",
+      subcategories: [
+        {
+          id: 1,
+          name: "Subcategory 1.1",
+        },
+        {
+          id: 2,
+          name: "Subcategory 1.2",
+        },
+        {
+          id: 3,
+          name: "Subcategory 1.3",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Category 2",
+      categoryType: "EXPENSE",
+      subcategories: [
+        {
+          id: 1,
+          name: "Subcategory 2.1",
+        },
+        {
+          id: 2,
+          name: "Subcategory 2.2",
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Category 3",
+      categoryType: "EXPENSE",
+      subcategories: [
+        {
+          id: 1,
+          name: "Subcategory 3.1",
+        },
+        {
+          id: 2,
+          name: "Subcategory 3.2",
+        },
+        {
+          id: 3,
+          name: "Subcategory 3.3",
+        },
+        {
+          id: 2,
+          name: "Subcategory 3.4",
+        },
+      ],
+    },
+  ];
 
   return (
-    <div id="transaction">
-      <div>
+    <div>
+      <header className="flex justify-between">
         <h1 className="text-3xl pb-4">Category Query</h1>
-        <hr />
-        <ul>
-          <li>{category.id}</li>
-          <li>{category.name}</li>
-          <li>{category.categoryType}</li>
-          {/*           <li>{category.subcategories}</li> */}
-        </ul>
+        <Button title="New Category" className="text-sm bg-amber-500" />
+      </header>
+      <hr className="my-4" />
+      <div className="flex justify-between">
+        <div className="w-[50%]">
+          <h1 className="text-2xl pb-4">Income</h1>
+          {incomeCategories ? (
+            incomeCategories.map((category) => (
+              <ul key={category.id}>
+                {category.name}
+                {category.subcategories.map((subcategory) => (
+                  <li className="ml-10 py-2 list-disc" key={subcategory.id}>
+                    {subcategory.name}
+                  </li>
+                ))}
+              </ul>
+            ))
+          ) : (
+            <span> There are no Income Categories registered yet</span>
+          )}
+        </div>
+        <div className="  w-[50%]">
+          <h1 className="text-2xl pb-4">Expense</h1>
+          {expenseCategories ? (
+            expenseCategories.map((category) => (
+              <ul key={category.id}>
+                {category.name}
+                {category.subcategories.map((subcategory) => (
+                  <li className="ml-10 py-2 list-disc" key={subcategory.id}>
+                    {subcategory.name}
+                  </li>
+                ))}
+              </ul>
+            ))
+          ) : (
+            <span> There are no Income Categories registered yet</span>
+          )}
+        </div>
       </div>
     </div>
   );
