e929092a09df74b1a77bd3113833e5266f118b15
Erick Silva <ericksilvaredes@gmail.com>
2024-05-13 16:23:13 -0300

    config: React Router DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./pages/error-page";
+import User from "./routes/user";
+import Transaction from "./routes/transaction";
+import Tag from "./routes/tag";
+import Category from "./routes/category";
+import Account from "./routes/account";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/user",
+        element: <User />,
+      },
+      {
+        path: "/account",
+        element: <Account />,
+      },
+      {
+        path: "/category",
+        element: <Category />,
+      },
+      {
+        path: "/tag",
+        element: <Tag />,
+      },
+      {
+        path: "/transaction",
+        element: <Transaction />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
